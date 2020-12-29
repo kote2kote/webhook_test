2aed06448adcac1b4a40024998a4f6b8e0f2a56b
kote2 <kote2kote2@gmail.com>
2020-12-29 20:27:32 +0900

    きたこれ！

---
+++ static/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 設定
+ */
+define( 'LOG_FILE', dirname( __FILE__ ) . '/hook.log' );
+define( 'LOG_ERR', dirname( __FILE__ ) . '/hook-error.log' );
+define( 'SECRET_KEY', '09pulu07' );
+
+/**
+ * 実行するコマンド
+ */
+$commands = array(
+  'develop' => '',//developブランチ
+  'main'  => 'git -C /home/admin/web/webhook.kote2.tokyo/public_html/webhook_test pull' // mainブランチ
+);
