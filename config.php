71f237c27f38baad61bb72fb21462b38a75fc9d0
kote2 <kote2kote2@gmail.com>
2020-12-29 16:46:38 +0900

    my firest commit

---
+++ config.php
@@ -0,0 +1,14 @@
+/**
+ * 設定
+ */
+define( 'LOG_FILE', dirname( __FILE__ ) . '/hook.log' );
+define( 'LOG_ERR', dirname( __FILE__ ) . '/hook-error.log' );
+define( 'SECRET_KEY', '09pulu07' );
+
+/**
+ * 実行するコマンド
+ */
+$commands = array(
+  'develop' => '',//developブランチ
+  'main'  => '' // mainブランチ
+);
