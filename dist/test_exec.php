af6dc8c46dbc8cdd6d3fbfd986f9d65be3a8f3ef
kote2 <kote2kote2@gmail.com>
2020-12-30 11:00:37 +0900

    よんよん

---
+++ dist/test_exec.php
@@ -0,0 +1,11 @@
+<?php 
+echo "# exec関数\n";
+var_dump( exec('ls') ); // 引数なし
+var_dump( exec('ls', $out, $ret) ); // 引数あり
+print_r( $out );
+var_dump( $ret );
+ 
+echo "\n# system関数\n";
+var_dump( system('ls', $ret) );
+var_dump( $ret );
+ ?>
