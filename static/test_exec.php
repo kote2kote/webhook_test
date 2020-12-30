f6084e2d3c7e820c0992a527701f26d98857fad6
kote2 <kote2kote2@gmail.com>
2020-12-30 16:49:08 +0900

    test7

--- static/test_exec.php
+++ static/test_exec.php
@@ -1,14 +1,15 @@
 <?php 
-echo "# exec関数\n";
-var_dump( exec('ls') ); // 引数なし
-var_dump( exec('ls', $out, $ret) ); // 引数あり
-print_r( $out );
-var_dump( $ret );
+// echo "# exec関数\n";
+// var_dump( exec('ls') ); // 引数なし
+// var_dump( exec('ls', $out, $ret) ); // 引数あり
+// print_r( $out );
+// var_dump( $ret );
  
-echo "\n# system関数\n";
-var_dump( system('ls', $ret) );
-var_dump( $ret );
+// echo "\n# system関数\n";
+// var_dump( system('ls', $ret) );
+// var_dump( $ret );
 
-exec('git -C /home/admin/web/webhook.kote2.tokyo/public_html/webhook_test pull', $output);
-var_dump( $output );
+var_dump( exec( "git -C /home/admin/web/webhook.kote2.tokyo/public_html/webhook_test pull" ));
+var_dump( exec( "git -C /home/admin/web/webhook.kote2.tokyo/public_html/webhook_test status" ));
+
  ?>
